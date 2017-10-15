20d1cc95dae0ab26342f83d7109bd68f574fcd26
shafari102 <32181582+shafari102@users.noreply.github.com>
2017-10-14 20:10:40 -0700

    Web Portfolio
    Added Landing Page Project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation()
+
+var today = new Date();
+var hourNOW= today.getHours();
+var greeting;
+
+if (hourNOW > 18) {
+  greeting='Good Evening!';
+} else if (hourNow >12) { greeting='Good Afternoon!';
+} else {
+  greeting='Welcome!';
+}
+
+document.write('<h2>' + greeting + '</h2>');
